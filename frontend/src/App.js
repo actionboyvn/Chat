50f07f1e0db6f9a5207408552104ffc1a77eeb54
Van Hien Le <actionboyvn@gmail.com>
2023-12-20 19:43:21 +0100

    Initialize Chat project

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-20">
+    <h1 className="text-5xl font-bold text-neutral-50 p-2 bg-neutral-400 w-fit rounded-xl cursor-pointer">
+      Hello world!!
+    </h1>
+    </div>
+  )
+}
